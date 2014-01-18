90c465478db67490d32fccee84584913ea335b16
Kuci <kuci24@gmail.com>
2014-01-18 16:51:27 +0100

    Add ZoomView

--- PeriodicTable/src/main/java/com/frozendevs/periodic/table/view/ZoomView.java
+++ PeriodicTable/src/main/java/com/frozendevs/periodic/table/view/ZoomView.java
@@ -1,7 +1,33 @@
 package com.frozendevs.periodic.table.view;
 
-/**
- * Created by kuci on 1/18/14.
- */
-public class ZoomView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewTreeObserver;
+import android.widget.FrameLayout;
+
+public class ZoomView extends FrameLayout implements ViewTreeObserver.OnGlobalLayoutListener {
+
+    public ZoomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ZoomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ZoomView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        getViewTreeObserver().addOnGlobalLayoutListener(this);
+    }
+
+    @Override
+    public void onGlobalLayout() {
+
+    }
 }
