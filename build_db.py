862718b813e05c0d1605944a9f4bef3521dfc838
Kuci <kuci24@gmail.com>
2014-08-02 15:37:54 +0200

    build_db.py: Add weight, category, group, period and block properties

--- build_db.py
+++ build_db.py
@@ -16,11 +16,15 @@ URL_PREFIX = 'http://en.wikipedia.org'
 def replace_chars(string, charset1, charset2):
     return ''.join(dict(zip(charset1, charset2)).get(c, c) for c in string)
 
-def get_property(content, name):
+def get_property(content, name, default = ''):
     for prop in content:
         if prop.strip().startswith(name + '='):
-            return prop.strip()[len(name) + 1:]
-    return ''
+            value = prop.strip()[len(name) + 1:]
+            if value.lower() != 'unknown' and value.lower() != 'n/a':
+                return value
+            else:
+                break
+    return default
 
 def signal_handler(signal, frame):
     print('\nFetching cancelled by user.')
@@ -29,7 +33,7 @@ def signal_handler(signal, frame):
 def fetch(url):
     print('Parsing properties from ' + url)
 
-    content = etree.parse(url).xpath("//*[local-name()='text']/text()")[0]
+    content = re.sub(r'<.?includeonly[^>]*>', '', etree.parse(url).xpath("//*[local-name()='text']/text()")[0])
     start = content.lower().index('{{infobox element') + 17
     content = HTMLParser().unescape(content[start:content.index('}}<noinclude>', start)]).split('\n|')
 
@@ -41,13 +45,33 @@ def fetch(url):
 
     name = get_property(content, 'name').capitalize()
 
+    weight = replace_chars(get_property(content, 'atomic mass'), '()', '[]')
+    try:
+        weight = format(float(weight), '.3f').rstrip('0').rstrip('.')
+    except ValueError:
+        pass
+
+    category = get_property(content, 'series').capitalize()
+
+    group = get_property(content, 'group', '3')
+
+    period = get_property(content, 'period')
+
+    block = get_property(content, 'block')
+
     element = {
         'number': number,
         'symbol': symbol,
-        'name': name
+        'name': name,
+        'weight': weight,
+        'category': category,
+        'group': group,
+        'period': period,
+        'block': block
     }
 
-    print(element)
+    #print(element)
+    print(element['block'])
 
     return element
 
