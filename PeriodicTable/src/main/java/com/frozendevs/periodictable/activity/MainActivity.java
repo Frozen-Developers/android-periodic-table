2d14076d849292244240ee9e7e37bc7566bccf2d
Kuci <kuci24@gmail.com>
2014-11-16 19:18:22 +0100

    MainActivity: Use TabsAdapter

--- PeriodicTable/src/main/java/com/frozendevs/periodictable/activity/MainActivity.java
+++ PeriodicTable/src/main/java/com/frozendevs/periodictable/activity/MainActivity.java
@@ -3,11 +3,8 @@ package com.frozendevs.periodictable.activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
@@ -19,74 +16,28 @@ import com.frozendevs.periodictable.R;
 import com.frozendevs.periodictable.fragment.ElementsFragment;
 import com.frozendevs.periodictable.fragment.TableFragment;
 import com.frozendevs.periodictable.model.adapter.ElementsAdapter;
-import com.frozendevs.periodictable.view.NonSwipeableViewPager;
+import com.frozendevs.periodictable.model.adapter.TabsAdapter;
 
-public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+public class MainActivity extends ActionBarActivity {
 
-    private NonSwipeableViewPager mViewPager;
-
-    private class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            switch (i) {
-                case 0:
-                    return new ElementsFragment();
-
-                case 1:
-                    return new TableFragment();
-            }
-
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return getString(R.string.fragment_title_elements);
-
-                case 1:
-                    return getString(R.string.fragment_title_table);
-            }
-
-            return null;
-        }
-    }
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main_activity);
 
-        final ActionBar actionBar = getSupportActionBar();
+        ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.pager);
 
-        mViewPager = (NonSwipeableViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(pagerAdapter);
-
-        mViewPager.setOnPageChangeListener(new NonSwipeableViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        for (int i = 0; i < pagerAdapter.getCount(); i++) {
-            actionBar.addTab(actionBar.newTab().setText(pagerAdapter.getPageTitle(i)).
-                    setTabListener(this));
-        }
+        TabsAdapter tabsAdapter = new TabsAdapter(this, mViewPager);
+        tabsAdapter.addTab(actionBar.newTab().setText(R.string.fragment_title_elements),
+                ElementsFragment.class, null);
+        tabsAdapter.addTab(actionBar.newTab().setText(R.string.fragment_title_isotopes),
+                TableFragment.class, null);
     }
 
     @Override
@@ -146,19 +97,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
     @Override
